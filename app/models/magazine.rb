c209bd98b98682694702965a1ad80be20c099643
Kyle Schechter <kyle.schechter@powerhrg.com>
2021-08-31 10:12:27 -0400

    done

--- app/models/magazine.rb
+++ app/models/magazine.rb
@@ -1,3 +1,13 @@
 class Magazine < ActiveRecord::Base
-  
+  has_many :subscriptions
+  has_many :readers, through: :subscriptions
+
+  def email_list
+    readers.map {|read| read.email}.join(';')
+  end
+
+  def self.most_popular
+    count = self.all.map {|mag| mag.subscriptions.count}.max
+    self.all.find {|mag| mag.subscriptions.count == count}
+  end
 end
--- app/models/reader.rb
+++ app/models/reader.rb
@@ -1,3 +1,16 @@
 class Reader < ActiveRecord::Base
-  
+  has_many :subscriptions
+  has_many :magazines, through: :subscriptions
+
+  def subscribe mag, pri
+    Subscription.create(reader_id: self.id, magazine_id: mag.id, price: pri)
+  end
+
+  def total_subscription_price
+    subscriptions.sum(:price)
+  end
+
+  def cancel_subscription mag
+    Subscription.find_by(reader_id: self.id, magazine_id: mag.id).destroy
+  end
 end
